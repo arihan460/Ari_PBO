8d36ea46e565726f19f65dc2dbb148fb4d908b20
arihan460 <134485997+arihan460@users.noreply.github.com>
2023-06-16 07:06:07 +0700

    MainKinetic.java

--- MainKinetic.java
+++ MainKinetic.java
@@ -8,39 +8,48 @@ package w04_encapsulation;
  *
  * @author SPACE
  */
-public class MainKinetic {
-    int massa, kecepatan;
+import java.util.Scanner;
 
-    public int getMassa() {
+public class MainKinetic {
+    double massa, kecepatan;
+
+    public double getMassa() {
         return massa;
     }
 
-    public void setMassa(int massa) {
+    public void setMassa(double massa) {
         this.massa = massa;
     }
 
-    public int getKecepatan() {
+    public double getKecepatan() {
         return kecepatan;
     }
 
-    public void setKecepatan(int kecepatan) {
+    public void setKecepatan(double kecepatan) {
         this.kecepatan = kecepatan;
     }
     
     public double hitungEnergiKinetic(){
-        double energiKinetic = 0.5 * massa * kecepatan * kecepatan;
+        double energiKinetic = 0.5 * massa * Math.pow(kecepatan, 2);
         return energiKinetic;
     }
     
     public static void main(String[] args) {
-        MainKinetic sepeda = new MainKinetic();
-        sepeda.setMassa(20);
-        sepeda.setKecepatan(2);
+        MainKinetic main = new MainKinetic();
+        Scanner scanner = new Scanner(System.in);
         
-        double energiKinetic = sepeda.hitungEnergiKinetic();
+        System.out.println("Masukkan massa benda (kg)= ");
+        double massa = scanner.nextDouble();
+        main.setMassa(massa);
         
-        System.out.println(sepeda.getMassa());
-        System.out.println(sepeda.getKecepatan());
-        System.out.println(energiKinetic);
+        System.out.println("Masukkan kecepatan benda (m/s) = ");
+        double kecepatan = scanner.nextDouble();
+        main.setKecepatan(kecepatan);
+        
+        double energiKinetik = main.hitungEnergiKinetic();
+        
+        System.out.println("Energi kinetik : "+ energiKinetik+" Joule");
+        
+        scanner.close();
     }
 }
